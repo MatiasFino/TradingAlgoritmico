b06a8753d1ae053727a3a46f571d1c245e127e35
MatiasFino <mfino@alumnos.exa.unicen.edu.ar>
2023-12-01 03:53:42 -0300

    todo funcionando, solo mal ponderados los indicadores

--- MyStrategy.py
+++ MyStrategy.py
@@ -1,3 +1,4 @@
+import datetime
 import math
 
 import backtrader as bt
@@ -16,6 +17,8 @@ class MyStrategy(bt.Strategy):
         ("rsi_mid", 50),
         ("stop_loss", 0.02),
         ("take_profit", 0.05),
+        ("start_date", datetime.datetime(2000, 1, 1)),
+        ("end_date", datetime.datetime(2004, 12, 31)),
     )
 
     def log(self, txt, dt=None):
@@ -23,6 +26,8 @@ class MyStrategy(bt.Strategy):
         print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
+        self.order_status = False
+        self.highest_price_so_far = 0
         self.order = None
         self.fast_ma = bt.indicators.SMA(self.data.close, period=self.p.fast_length)
         self.slow_ma = bt.indicators.SMA(self.data.close, period=self.p.slow_length)
@@ -34,73 +39,105 @@ class MyStrategy(bt.Strategy):
         self.momentum = bt.indicators.MomentumOscillator()
         self.macd = bt.indicators.MACD()
         self.in_position = False
-        self.trail_percent = 0.05
+        self.stop_loss = 0.95
+        self.order_size = 500
 
     def next(self):
         score = 0
         indicators = []
+
         if not self.in_position:
-            if self.fast_ma.sma[0] > self.slow_ma.sma[0] > self.really_slow_ma.sma[0]:
+            if self.fast_ma.sma[0] > self.slow_ma.sma[0] > self.really_slow_ma.sma[0] and (self.fast_ma.sma[-1]
+                                                                                           < self.slow_ma.sma[-1] or
+                                                                                           self.really_slow_ma.sma[-1] >
+                                                                                           self.slow_ma.sma[-1]):
                 if self.adx.adx[0] > 40:
                     indicators.append("Adx > 40 & triple cruce de medias")
-                    score += 6
+                    score += 50
                 if self.adx.adx[0] > 20:
                     indicators.append("Adx > 20 & triple cruce de medias")
-                    score += 3
-                # Buy Signal
-                # self.in_position = True
+                    score += 15
+
             else:
-                if self.fast_ma.sma > self.slow_ma.sma:
+                if self.fast_ma.sma > self.slow_ma.sma and self.fast_ma.sma[-1] < self.slow_ma.sma[-1]:
                     if self.adx.adx[0] > 40:
                         indicators.append("Adx > 40 & doble cruce de medias")
-                        score += 4
+                        score += 30
                     if self.adx.adx[0] > 20:
                         indicators.append("Adx > 20 & triple cruce de medias")
-                        score += 2
+                        score += 5
             if self.data0.close[0] > self.bollinger.mid[0] > self.data0.close[-1]:
                 indicators.append("cruce al alza de la banda de bollinger")
-                score += 4
+                score += 10
             # if self.data0.close[-2] < self.bollinger.mid[0] and self.data.close[-3] < self.bollinger.mid[0]:
-              #   score += 2
+            #   score += 2
             if self.data0.close[0] > self.parabolic_sar:
                 indicators.append("Parabolico marca tendencia alcista")
-                score += 1
+                score += 5
             if self.data0.close[-1] < self.parabolic_sar and self.data0.close[-2] < self.parabolic_sar:
                 indicators.append("Cambio de tendencia bajista a alcista segun Parabolico")
-                score += 4
-            if self.adx.DIplus[0] > self.adx.DIminus[0] and self.adx.DIplus[1] < self.adx.DIminus[1]:
+                score += 25
+            if self.adx.DIplus[0] > self.adx.DIminus[0] and self.adx.DIplus[-1] < self.adx.DIminus[-1]:
                 if self.adx.adx[0] >= 20:
                     indicators.append("Adx >= 20 y +DI corta al alza por encima de -DI")
-                    score += 5
+                    score += 10
                 if self.adx.adx[0] >= 40:
                     indicators.append("Adx >= 40 y +DI corta al alza por encima de -DI")
-                    score += 10
+                    score += 30
             if self.momentum[0] > 0 >= self.momentum[-1]:
                 indicators.append("Momentum al alza")
-                score += 4
+                score += 10
             if self.data0.close[0] > self.data0.close[-1]:
                 if self.rsi.rsi[-1] < 30 < self.rsi.rsi[0]:
                     indicators.append("RSI rompe la linea de sobre venta al alza")
-                    score += 7
+                    score += 20
                 if self.rsi.rsi[0] >= 50:
                     indicators.append("RSI marca tendencia al alza con relativamente buena fuerza")
-                    score += 2
+                    score += 10
             if self.macd.macd[0] > 0 > self.macd.macd[-1]:
                 indicators.append("MACD cruza la barrera del 0 al alza")
-                score += 6
+                score += 25
             if self.macd.macd[0] > self.macd.lines[0]:
                 indicators.append("MACD corta al alza la linea de señal")
-                score += 2
+                score += 10
             print("score: {}".format(score))
             print(indicators)
-            if score >= 6:
-                buy_price = self.data0.close[0] if not math.isnan(self.data0.close[0]) else None
-                self.order = self.buy(data=self.data0, price=buy_price, size=2,
-                                      exectype=bt.Order.StopTrail,
-                                      trailpercent=self.trail_percent, parent=self.sell(
-                        price=buy_price,
-                        exectype=bt.Order.Stop
-                    ))
+            if score >= 50:
+
+                if not self.in_position:  # and self.data0.datetime.datetime(0) < self.p.end_date - 2:
+                    print("nuevo hsf")
+                    self.highest_price_so_far = self.data0.close[0]
+                    print("highest price so far cuando compro {}".format(self.highest_price_so_far))
+                    # Lógica para abrir una posición de compra
+                    buy_price = self.data0.close[0] if not math.isnan(self.data0.close[0]) else None
+                    self.order = self.buy(data=self.data0, price=buy_price, size=self.order_size)
+
+                    # Calcular el precio de stop loss (venta) como porcentaje de la orden de compra
+
+                    # Crear una orden de venta (stop loss) asociada a la orden de compra
+                    # self.trail_order = self.sell(
+                    #     data=self.data0,
+                    #     price=trail_price,
+                    #     parent=self.order,
+                    #     exectype=bt.Order.Stop
+                    # )
+
+        if self.in_position:
+            # if self.data0.datetime.datetime(0) == self.p.end_date - 1:
+            # self.sell(size=self.order_size)
+            if self.data0.close[0] > self.highest_price_so_far:
+                self.highest_price_so_far = self.data0.close[0]
+            print("highest price so far en posicion {}".format(self.highest_price_so_far))
+            print("fecha {}".format(self.data0.datetime.datetime(0)))
+            print("precio actual {}".format(self.data0.close[0]))
+            print("{}% {}".format(self.stop_loss, self.highest_price_so_far * self.stop_loss))
+            # Lógica para gestionar la posición existente
+            # Aquí puedes agregar lógica para tomar beneficios, etc.
+
+            # Ejemplo de cerrar la posición después de cierto número de días
+            if self.data0.close[0] <= self.highest_price_so_far * self.stop_loss:
+                self.log("Selling with stop loss at 5% less than highest price")
+                self.sell(size=self.order_size)
 
     def notify_order(self, order):
         if order.status is order.Completed:
@@ -115,9 +152,11 @@ class MyStrategy(bt.Strategy):
             if order.isbuy():
                 self.log('Se acepto la orden COMPRA de id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
                                                                                  order.created.price))
+                self.in_position = True  # Establecer la posición como abierta
             elif order.issell():
                 self.log('Se acepto la orden VENTA de id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
                                                                                 order.created.price))
+                self.in_position = False
             self.cancel(order)
 
         if order.status is order.Submitted:
--- main.py
+++ main.py
@@ -14,12 +14,14 @@ if __name__ == '__main__':
     # Create a cerebro entity
     cerebro = bt.Cerebro()
     # Create a Data Feed
+    start_date = datetime.datetime(2000, 1, 1)
+    end_date = datetime.datetime(2010, 12, 31)
     data = bt.feeds.YahooFinanceCSVData(
         dataname='datas/orcl-1995-2014.txt',
         # Do not pass values before this date
-        fromdate=datetime.datetime(2000, 1, 1),
+        fromdate=start_date,
         # Do not pass values after this date
-        todate=datetime.datetime(2000, 12, 31),
+        todate=end_date,
         reverse=False,
     )
 
@@ -36,6 +38,7 @@ if __name__ == '__main__':
 
     # Run over everything
     cerebro.run()
-    #cerebro.plot()
-    # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.plot()
+    # Print out the final result
