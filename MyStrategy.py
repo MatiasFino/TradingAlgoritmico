3a0886a4089ba8f4d6897bc88e76c2a7d514ddeb
Bricio Vellaz <briciovellaz@gmail.com>
2023-12-01 15:23:07 -0300

    Translate info logs to english

--- MyStrategy.py
+++ MyStrategy.py
@@ -50,62 +50,62 @@ class MyStrategy(bt.Strategy):
                                                                                            self.really_slow_ma.sma[-1] >
                                                                                            self.slow_ma.sma[-1]):
                 if self.adx.adx[0] > 40:
-                    indicators.append("Adx > 40 & triple cruce de medias")
+                    indicators.append("Adx > 40 & triple moving average crossover")
                     score += 50
                 if self.adx.adx[0] > 20:
-                    indicators.append("Adx > 20 & triple cruce de medias")
+                    indicators.append("Adx > 20 & triple moving average crossover")
                     score += 15
 
             else:
                 if self.fast_ma.sma > self.slow_ma.sma and self.fast_ma.sma[-1] < self.slow_ma.sma[-1]:
                     if self.adx.adx[0] > 40:
-                        indicators.append("Adx > 40 & doble cruce de medias")
+                        indicators.append("Adx > 40 & dual moving average crossover")
                         score += 30
                     if self.adx.adx[0] > 20:
-                        indicators.append("Adx > 20 & triple cruce de medias")
+                        indicators.append("Adx > 20 & triple moving average crossover")
                         score += 5
             if self.data0.close[0] > self.bollinger.mid[0] > self.data0.close[-1]:
-                indicators.append("cruce al alza de la banda de bollinger")
+                indicators.append("Bollinger Band breakout to the upside")
                 score += 10
             # if self.data0.close[-2] < self.bollinger.mid[0] and self.data.close[-3] < self.bollinger.mid[0]:
             #   score += 2
             if self.data0.close[0] > self.parabolic_sar:
-                indicators.append("Parabolico marca tendencia alcista")
+                indicators.append("Parabolic SAR signals bullish trend")
                 score += 5
             if self.data0.close[-1] < self.parabolic_sar and self.data0.close[-2] < self.parabolic_sar:
-                indicators.append("Cambio de tendencia bajista a alcista segun Parabolico")
+                indicators.append("Switch from bearish to bullish trend according to Parabolic")
                 score += 25
             if self.adx.DIplus[0] > self.adx.DIminus[0] and self.adx.DIplus[-1] < self.adx.DIminus[-1]:
                 if self.adx.adx[0] >= 20:
-                    indicators.append("Adx >= 20 y +DI corta al alza por encima de -DI")
+                    indicators.append("Adx >= 20 and +DI crosses above -DI")
                     score += 10
                 if self.adx.adx[0] >= 40:
-                    indicators.append("Adx >= 40 y +DI corta al alza por encima de -DI")
+                    indicators.append("Adx >= 40 and +DI crosses above -DI")
                     score += 30
             if self.momentum[0] > 0 >= self.momentum[-1]:
-                indicators.append("Momentum al alza")
+                indicators.append("Bullish momentum")
                 score += 10
             if self.data0.close[0] > self.data0.close[-1]:
                 if self.rsi.rsi[-1] < 30 < self.rsi.rsi[0]:
-                    indicators.append("RSI rompe la linea de sobre venta al alza")
+                    indicators.append("RSI breaks out of oversold territory to the upside")
                     score += 20
                 if self.rsi.rsi[0] >= 50:
-                    indicators.append("RSI marca tendencia al alza con relativamente buena fuerza")
+                    indicators.append("RSI trends upward with relatively good strength")
                     score += 10
             if self.macd.macd[0] > 0 > self.macd.macd[-1]:
-                indicators.append("MACD cruza la barrera del 0 al alza")
+                indicators.append("MACD breaks through the 0 line to the upside")
                 score += 25
             if self.macd.macd[0] > self.macd.lines[0]:
-                indicators.append("MACD corta al alza la linea de señal")
+                indicators.append("MACD crosses to the upside over the signal line")
                 score += 10
             print("score: {}".format(score))
             print(indicators)
             if score >= 20:
 
                 if not self.in_position:  # and self.data0.datetime.datetime(0) < self.p.end_date - 2:
-                    print("nuevo hsf")
+                    print("New hsf")
                     self.highest_price_so_far = self.data0.close[0]
-                    print("highest price so far cuando compro {}".format(self.highest_price_so_far))
+                    print("Highest price so far {}".format(self.highest_price_so_far))
                     # Lógica para abrir una posición de compra
                     buy_price = self.data0.close[0] if not math.isnan(self.data0.close[0]) else None
                     self.order = self.buy(data=self.data0, price=buy_price, size=self.order_size)
@@ -125,9 +125,9 @@ class MyStrategy(bt.Strategy):
             # self.sell(size=self.order_size)
             if self.data0.close[0] > self.highest_price_so_far:
                 self.highest_price_so_far = self.data0.close[0]
-            print("highest price so far en posicion {}".format(self.highest_price_so_far))
-            print("fecha {}".format(self.data0.datetime.datetime(0)))
-            print("precio actual {}".format(self.data0.close[0]))
+            print("Highest price so far in position {}".format(self.highest_price_so_far))
+            print("Date {}".format(self.data0.datetime.datetime(0)))
+            print("Actual price {}".format(self.data0.close[0]))
             print("{}% {}".format(self.stop_loss, self.highest_price_so_far * self.stop_loss))
             # Lógica para gestionar la posición existente
             # Aquí puedes agregar lógica para tomar beneficios, etc.
@@ -140,27 +140,27 @@ class MyStrategy(bt.Strategy):
     def notify_order(self, order):
         if order.status is order.Completed:
             if order.isbuy():
-                self.log('Se completo la orden COMPRA de id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
+                self.log('BUY order COMPLETED with id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
                                                                                    order.executed.price))
             elif order.issell():
-                self.log('Se completo la orden VENTA de id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
+                self.log('SELL order COMPLETED with id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
                                                                                   order.executed.price))
 
         if order.status is order.Accepted:
             if order.isbuy():
-                self.log('Se acepto la orden COMPRA de id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
+                self.log('BUY order ACCEPTED with id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
                                                                                  order.created.price))
                 self.in_position = True  # Establecer la posición como abierta
             elif order.issell():
-                self.log('Se acepto la orden VENTA de id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
+                self.log('SELL order ACCEPTED with id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
                                                                                 order.created.price))
                 self.in_position = False
             self.cancel(order)
 
         if order.status is order.Submitted:
             if order.isbuy():
-                self.log('Se envio la orden COMPRA de id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
+                self.log('BUY order SENT with id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
                                                                                 order.created.price))
             elif order.issell():
-                self.log('Se envio la orden VENTA de id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
+                self.log('SELL order SENT with id: {} [{} @ ${}]'.format(order.ref, abs(order.size),
                                                                                order.created.price))
